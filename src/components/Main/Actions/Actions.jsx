4ca158f8518b15d1e706adc8b6e3411123309801
Maksim Shamal <max280599@gmail.com>
2020-08-02 12:18:20 +0300

    delete gh-pages branch files

--- src/components/Main/Actions/Actions.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Actions.scss";
-
-export default function Actions() {
-  return <div className="actions-container"></div>;
-}
--- src/store/reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { usersReducer } from "./users/reducers";
-
-export default combineReducers({
-  users: usersReducer,
-});
