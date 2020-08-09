efd6eb7277247227e10da08af04f46bc05b5f9df
Maksim Shamal <max280599@gmail.com>
2020-08-09 20:09:47 +0300

    before change scrollbar at user container

---
+++ src/components/Main/Users/RoomSwitcher/RoomSwitcher.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './RoomSwitcher.scss'
+
+export default function RoomSwitcher() {
+  return (
+    <div className="room-switches-container">
+      <div className="room-switch"></div>
+      <div className="room-switch"></div>
+      <div className="room-switch"></div>
+      <div className="room-switch"></div>
+      <div className="room-switch"></div>
+    </div>
+  )
+}
--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react'
 
-import { useDrop } from "react-dnd";
-import { ItemTypes } from "utilities/items";
+import { useDrop } from 'react-dnd'
+import { ItemTypes } from 'utilities/items'
 
-import "./Users.scss";
-import SearchUsers from "./SearchUsers/SearchUsersRedux";
-import User from "./User/User";
+import './Users.scss'
+import SearchUsers from './SearchUsers/SearchUsersRedux'
+import RoomSwitcher from './RoomSwitcher/RoomSwitcher'
+import User from './User/User'
 
 export default function Users(props) {
-  const { users, activeUsersIds, filterValue, setUserInactive } = props;
+  const { users, activeUsersIds, filterValue, setUserInactive } = props
   const [{ isOver, item }, drop] = useDrop({
     accept: ItemTypes.CARD,
     drop: (item, monitor) => setUserInactive(item.id),
@@ -16,16 +17,17 @@ export default function Users(props) {
       isOver: monitor.isOver(),
       item: monitor.getItem(),
     }),
-  });
+  })
   return (
     <div
-      className={`users-container ${
-        isOver && activeUsersIds.includes(item?.id) ? "droppable--inactive" : ""
-      }`}
+      className={`users-container ${isOver && activeUsersIds.includes(item?.id) ? 'droppable--inactive' : ''}`}
       ref={drop}
-    >
-      <SearchUsers placeholder="Find user..." />
-      <div className="users-block">
+    > 
+      <div className="users--header">
+        <SearchUsers placeholder="Find user..." />
+        <RoomSwitcher />
+      </div>
+      <div className="users--main">
         {Object.values(users)
           .filter(({ name }) => name.startsWith(filterValue))
           .map(({ name, id, icon }) => (
@@ -33,5 +35,5 @@ export default function Users(props) {
           ))}
       </div>
     </div>
-  );
+  )
 }
