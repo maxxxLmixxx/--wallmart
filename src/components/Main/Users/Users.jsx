78ef59deeb8fc92c918d68197ca7cbe11250e5c2
Maksim Shamal <max280599@gmail.com>
2020-08-23 12:47:41 +0300

    change border style

--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback } from 'react'
 
-import { useDrop } from "react-dnd";
-import { ItemTypes } from "utilities/items";
+import { useDrop } from 'react-dnd'
+import { ItemTypes } from 'utilities/items'
 
-import "./Users.scss";
-import SearchUsers from "./SearchUsers/SearchUsersRedux";
-import RoomSwitcher from "./RoomSwitcher/RoomSwitcher";
-import User from "./User/UserRedux";
+import './Users.scss'
+import SearchUsers from './SearchUsers/SearchUsersRedux'
+import RoomSwitcher from './RoomSwitcher/RoomSwitcher'
+import User from './User/UserRedux'
 
 export default function Users(props) {
-  const { users, activeUsersIds, filterValue, setUserInactive } = props;
+  const { users, activeUsersIds, filterValue, setUserInactive } = props
   const [{ isOver, item }, drop] = useDrop({
     accept: ItemTypes.CARD,
     drop: (item, monitor) => setUserInactive(item.id),
@@ -17,38 +17,30 @@ export default function Users(props) {
       isOver: monitor.isOver(),
       item: monitor.getItem(),
     }),
-  });
+  })
 
   //==============================================================
-  const [isShift, setShift] = useState(false);
-  const handleShiftDown = useCallback(
-    ({ shiftKey }) => {
-      if (shiftKey && !isShift) setShift(true);
-    },
-    []
-  );
-  const handleShiftUp = useCallback(
-    ({ shiftKey }) => {
-      if (shiftKey && isShift) setShift(false);
-    },
-    []
-  );
+  const [isShift, setShift] = useState(false)
+  const handleShiftDown = useCallback(({ key }) => {
+    if (key === 'Shift') setShift(true)
+  }, [])
+  const handleShiftUp = useCallback(({ key }) => {
+    if (key === 'Shift') setShift(false)
+  }, [])
 
   useEffect(() => {
-    window.addEventListener("keydown", handleShiftDown);
-    window.addEventListener("keyup", handleShiftUp);
+    window.addEventListener('keydown', handleShiftDown)
+    window.addEventListener('keyup', handleShiftUp)
     return () => {
-      window.removeEventListener("keydown", handleShiftUp);
-      window.removeEventListener("keyup", handleShiftDown);
-    };
-  }, [handleShiftUp, handleShiftDown]);
+      window.removeEventListener('keydown', handleShiftUp)
+      window.removeEventListener('keyup', handleShiftDown)
+    }
+  }, [handleShiftUp, handleShiftDown])
   //==============================================================
 
   return (
     <div
-      className={`users-container ${
-        isOver && activeUsersIds.includes(item?.id) ? "droppable--inactive" : ""
-      }`}
+      className={`users-container ${isOver && activeUsersIds.includes(item?.id) ? 'droppable--inactive' : ''}`}
       ref={drop}
     >
       <div className="users--header">
@@ -63,5 +55,5 @@ export default function Users(props) {
           ))}
       </div>
     </div>
-  );
+  )
 }
