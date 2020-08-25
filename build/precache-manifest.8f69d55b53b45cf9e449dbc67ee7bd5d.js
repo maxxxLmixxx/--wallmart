75f222d4fc2f4c09359e4879fa2916f336279a6e
maxxxLmixxx <max280599@gmail.com>
2020-08-25 22:36:01 +0300

    C:/Program Files/Git/build v-0.5-beta

---
+++ build/precache-manifest.8f69d55b53b45cf9e449dbc67ee7bd5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0450d8792c4a7162cc23d9201efa5a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2105d06d28f30479a01",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f528d463e3d33fc0ab08",
+    "url": "/static/css/main.2a32aebd.chunk.css"
+  },
+  {
+    "revision": "e2105d06d28f30479a01",
+    "url": "/static/js/2.2bc1e8bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2bc1e8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f528d463e3d33fc0ab08",
+    "url": "/static/js/main.b61b89a3.chunk.js"
+  },
+  {
+    "revision": "51c3b397c36e8e04e03f",
+    "url": "/static/js/runtime-main.fd1d0e2e.js"
+  },
+  {
+    "revision": "85a7cb3ea417d4d9119e8c5c4dac87c9",
+    "url": "/static/media/hole.85a7cb3e.svg"
+  }
+]);
