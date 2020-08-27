eddc3a85468807dba3dc17b1d5f337281106a706
maxxxLmixxx <max280599@gmail.com>
2020-08-27 21:10:14 +0300

    add tasks at header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,14 @@ import './Header.scss'
 export default function Header() {
     return (
         <header>
-            
+            1. save to localstorage
+            2. add actions
+            3. add update action
+            4. roomFilter
+            5. add personal actions
+            6. connectivity with server
+            7. шлифовка
+            8. red status 
         </header>
     )
 }
