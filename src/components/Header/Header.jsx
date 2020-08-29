c61fa4cf3d5312431889cdfef5649158dfda66b0
Maksim Shamal <max280599@gmail.com>
2020-08-29 22:37:30 +0300

    m

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import './Header.scss'
 
 export default function Header() {
-    return (
-        <header>
-            1. save to localstorage
-            2. add update action
-            3. roomFilter
-            4. connectivity with server
-            5. шлифовка
-        </header>
-    )
+  return (
+    <header>
+      1. save to localstorage 2. add update action 3. roomFilter 4. connectivity with server 5. шлифовка <br />{' '}
+      PASSWORD: 159854673102589754steam1356fsaxgjupk,ohbrplrp[rpoohea5l-srtn,=pgr,owmok[yjngk,o
+      ,emtclp=gtksg4e[ool=aflpkq99849kogo,oeg451t1g5rt1h45t3r22
+    </header>
+  )
 }
