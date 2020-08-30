77fa9978d071010a18b10988eb4232fdd2909d38
maxxxLmixxx <max280599@gmail.com>
2020-08-30 08:47:28 +0300

    m

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import './Header.scss'
+import React from "react";
+import "./Header.scss";
 
 export default function Header() {
   return (
     <header>
-      1. save to localstorage 2. add update action 3. roomFilter 4. connectivity with server 5. шлифовка <br />{' '}
-      PASSWORD: 159854673102589754steam1356fsaxgjupk,ohbrplrp[rpoohea5l-srtn,=pgr,owmok[yjngk,o
-      ,emtclp=gtksg4e[ool=aflpkq99849kogo,oeg451t1g5rt1h45t3r22
+      1. room filter 2. font ban 3. save 4. сортировку по статусу 5. server --
+      list [input] || server--list add
     </header>
-  )
+  );
 }
