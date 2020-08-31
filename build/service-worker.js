8c24f9aeaa1732896c092c739c7f9dfe14feeb30
maxxxLmixxx <max280599@gmail.com>
2020-08-31 20:55:14 +0300

    to deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7dccd1e48759f268b7b5a6e4e14ab742.js"
+  "/precache-manifest.d8c18d9c07fee102acc5fa977ca8d838.js"
 );
 
 self.addEventListener('message', (event) => {
